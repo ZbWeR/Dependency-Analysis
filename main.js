28b90ff92e7c91da43270be08608c70813a0929f
ZbWeR <1641064392@qq.com>
2023-07-28 01:26:32 +0800

    ✨ feat: 获取依赖冲突相关信息

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const readStream = fs.createReadStream('package-lock.json', { encoding: 'utf8' }
 
 let plainData = '';
 let packages = null;
+let conflictPackages = {};
 let isVisit = {};
 
 readStream.on('data', (chunk) => {
@@ -21,26 +22,46 @@ readStream.on('end', () => {
     });
     // 提取 packages 空字符串对象中的依赖信息
     let directDependencies = packages?.[""]?.["dependencies"];
-    isVisit = {};
-    fs.writeFile('dependency.txt', JSON.stringify(dfs('glob')), () => { })
+    let resObj = {};
+    Object.keys(directDependencies).forEach((item) => {
+        isVisit = {};
+        resObj[item] = dfs(item);
+    })
+    fs.writeFile('dependency.json', JSON.stringify(resObj), (err) => {
+        if (err) console.error(err);
+        else console.log("-- dependency success --");
+    })
+    fs.writeFile('conflict.json', JSON.stringify(conflictPackages), (err) => {
+        if (err) console.error(err);
+        else console.log("-- conflict success --");
+    })
 })
 
 /**
-     * 深度优先搜索，生成依赖关系图
-     * @param {string} rootPackageName - 根包名
-     * @param {number} depth - 深度
-     * @param {string} prefix - 前缀
-     * @returns {string} - 依赖关系图字符串
-     */
-function dfs(rootPackageName, depth = 0, prefix = '') {
+ * 深度优先搜索，生成依赖关系图
+ * @param {string} rootPackageName - 根包名
+ * @param {number} depth - 深度
+ * @param {string} prefix - 前缀
+ * @returns {object} - 依赖关系对象
+ */
+function dfs(rootPackageName, depth = 0, prefix = 'NotFound') {
+    // 先查找前缀的node_modules目录中是否存在依赖
+    let payload = `${prefix}/node_modules/${rootPackageName}`;
+    let checkPackage = packages?.[payload];
+    let packageName = payload;
 
-    let payload = `${prefix ? prefix + "/node_modules/" : ""}${rootPackageName}`;
-    let checkPackage = packages?.[payload] || packages?.[rootPackageName];
-    let packageName = packages?.[payload] ? payload : rootPackageName;
-    // console.log(rootPackageName, packageName)
+    // 存在依赖冲突
+    if (checkPackage) {
+        conflictPackages[rootPackageName] = conflictPackages[rootPackageName] || {};
+        conflictPackages[rootPackageName][checkPackage.version] = conflictPackages[rootPackageName][checkPackage.version] || [];
+        conflictPackages[rootPackageName][checkPackage.version].push(prefix);
+    } else {
+        checkPackage = packages?.[rootPackageName];
+        packageName = rootPackageName
+    }
+
     // 处理循环引用的情况
     if (isVisit[packageName]) {
-        console.log(packageName);
         return {
             packageName,
             version: checkPackage.version,
@@ -48,9 +69,9 @@ function dfs(rootPackageName, depth = 0, prefix = '') {
             conflict: true
         }
     }
-    isVisit[packageName] = true;
 
-    let { version, dependencies } = packages?.[rootPackageName];
+    isVisit[packageName] = true;
+    let { version, dependencies } = checkPackage || {};
     let tmpObj = { packageName, version, dependencies: [] }
 
     // 递归检索依赖关系
